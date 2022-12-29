2aba482207e5c5c1551ed89c292ead713a6ccb81
Jr Gouveia <gouvik.dev@gmail.com.br>
2022-12-29 01:52:30 -0300

    adição da validação do form de moments e estilização dos erros

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,11 +1,59 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css']
 })
-export class MomentFormComponent {
-@Input() btnText!: string
+export class MomentFormComponent implements OnInit {
+  @Input() btnText!: string
+
+  momentForm!: FormGroup
+  descLength: number = 256;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      description: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(256),
+      ]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  descCountWords(): number {
+    const MAX_CHAR = 256;
+    const WORDS_LENGTH: number = this.momentForm.get('description')?.value.length;
+    let remaining: number = MAX_CHAR - WORDS_LENGTH;
+    return this.descLength = remaining;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+
+  submit(): void {
+    if(this.momentForm.invalid) {
+      return;
+    }
+
+    console.log('====================================');
+    console.log('Envio do Form');
+    console.log('====================================');
+  }
 
 }
